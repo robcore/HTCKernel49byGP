1579916f7ac730be50109c6e495f51e003819b9c
giampa64 <giampa64@gmail.com>
2015-08-26 11:25:43 +0200

    Added SmartassV2 & Lulzactive CPU Governors

--- HTCKernel49byGP/include/generated/compile.h
+++ HTCKernel49byGP/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Tue Aug 25 10:40:07 CEST 2015"
+#define UTS_VERSION "#12 SMP PREEMPT Wed Aug 26 11:08:09 CEST 2015"
 #define LINUX_COMPILE_BY "giampa"
 #define LINUX_COMPILE_HOST "giampa-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
