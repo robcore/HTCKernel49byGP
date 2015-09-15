970f53d97bc772f4bb3ac9304006bf73047df9d2
giampa64 <giampa64@gmail.com>
2015-09-15 17:27:20 +0200

    Added USB Fastcharge

--- HTCKernel49byGP/include/generated/compile.h
+++ HTCKernel49byGP/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 129 */
+/* This file is auto generated, version 130 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#129 SMP PREEMPT Tue Sep 15 15:57:03 CEST 2015"
+#define UTS_VERSION "#130 SMP PREEMPT Tue Sep 15 16:57:05 CEST 2015"
 #define LINUX_COMPILE_BY "giampa"
 #define LINUX_COMPILE_HOST "giampa-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.7.4 (crosstool-NG linaro-1.13.1+bzr2694 - Linaro GCC 2014.06 - Cortex-A15) "
