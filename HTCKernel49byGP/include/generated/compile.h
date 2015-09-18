91648e52afc161ab20cdfa47b4017d937e224720
giampa64 <giampa64@gmail.com>
2015-09-18 08:29:02 +0200

    Advanced USB Fastcharge & RAM optimization

--- HTCKernel49byGP/include/generated/compile.h
+++ HTCKernel49byGP/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 131 */
+/* This file is auto generated, version 134 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#131 SMP PREEMPT Wed Sep 16 09:37:44 CEST 2015"
+#define UTS_VERSION "#134 SMP PREEMPT Thu Sep 17 17:49:53 CEST 2015"
 #define LINUX_COMPILE_BY "giampa"
 #define LINUX_COMPILE_HOST "giampa-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.7.4 (crosstool-NG linaro-1.13.1+bzr2694 - Linaro GCC 2014.06 - Cortex-A15) "
