6c8f34f8df243730cad84603e859d387db1f3681
giampa64 <giampa64@gmail.com>
2015-09-23 10:08:34 +0200

    Spring cleaning

--- HTCKernel49byGP/include/generated/compile.h
+++ HTCKernel49byGP/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 146 */
+/* This file is auto generated, version 147 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#146 SMP PREEMPT Wed Sep 23 08:37:41 CEST 2015"
+#define UTS_VERSION "#147 SMP PREEMPT Wed Sep 23 10:07:09 CEST 2015"
 #define LINUX_COMPILE_BY "giampa"
 #define LINUX_COMPILE_HOST "giampa-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.7.4 (crosstool-NG linaro-1.13.1+bzr2694 - Linaro GCC 2014.06 - Cortex-A15) "
